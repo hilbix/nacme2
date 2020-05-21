0891b0d7589b6cfb6cd0b527c231d4c907eb9952
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-05-21 19:04:41 +0200

    init/main.sh added

---
+++ init/main.sh
@@ -0,0 +1,17 @@
+##: init takes no parameters
+##	This is only used for the very first initial setup.
+##	Use bin/check afterwards.
+
+x test -x bin/check && ERROR please use "$WORK/bin/check" instead
+
+x IGN tty || ERROR this must be run from a TTY as it requires user interaction
+CLEAN "$WORK" || ERROR "$WORK:" git status not clean
+CLEAN "$MAIN" || ERROR "$MAIN:" git status not clean
+
+if	x ign git status
+then
+	:
+fi
+
+NOTYET
+
