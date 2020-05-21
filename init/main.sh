80ea2f53803e953b5a5e212b0ccc635ea47776e2
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-05-21 19:08:18 +0200

    init/main.sh modified

--- init/main.sh
+++ init/main.sh
@@ -6,7 +6,7 @@ x test -x bin/check && ERROR please use "$WORK/bin/check" instead
 
 x IGN tty || ERROR this must be run from a TTY as it requires user interaction
 CLEAN "$WORK" || ERROR "$WORK:" git status not clean
-CLEAN "$MAIN" || ERROR "$MAIN:" git status not clean
+#CLEAN "$MAIN" || ERROR "$MAIN:" git status not clean
 
 if	x ign git status
 then
